6f1e5eef17ed71b69dac8a4c1616966b255e5046
Trevor Stow <trevorstow@gmail.com>
2010-03-16 16:09:01 -0400

    adding hoptoad notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
     include SecurityActions
 
-    helper :all # include all helpers, all the time
+    helper :all
     # protect_from_forgery # :secret => '2a090af85266822601d58b602bc63763'
 
-    # Scrub sensitive parameters from your log
     filter_parameter_logging :password, :password_confirmation
     
     before_filter :current_user
